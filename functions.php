e099c6524fdd1eb64527139fb50c367f9f626860
Mononchorcha <bulbulspb458@gmail.com>
2026-06-29 02:05:13 +0600

    Enqueue theme stylesheet

--- functions.php
+++ functions.php
@@ -4,3 +4,13 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+function mononchorcha_enqueue_styles() {
+    wp_enqueue_style(
+        'mononchorcha-style',
+        get_stylesheet_uri(),
+        array(),
+        wp_get_theme()->get( 'Version' )
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'mononchorcha_enqueue_styles' );
