6ddb1312c10d6ae8bbfc07433465ab28c2a9c34f
Mononchorcha <bulbulspb458@gmail.com>
2026-06-29 02:47:54 +0600

    Add basic theme supports

--- functions.php
+++ functions.php
@@ -14,3 +14,13 @@ function mononchorcha_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'mononchorcha_enqueue_styles' );
+function mononchorcha_theme_setup() {
+
+    add_theme_support( 'title-tag' );
+
+    add_theme_support( 'post-thumbnails' );
+
+    add_theme_support( 'custom-logo' );
+
+}
+add_action( 'after_setup_theme', 'mononchorcha_theme_setup' );
