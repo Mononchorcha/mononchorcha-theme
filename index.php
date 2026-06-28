aadcddf01f0d29b8724663e24b6cd25c920bbcbf
Mononchorcha <bulbulspb458@gmail.com>
2026-06-29 02:11:52 +0600

    Create basic homepage

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php // Silence is golden.
+<?php get_header(); ?>
+
+<main class="site-main">
+    <h1>মননচর্চা</h1>
+    <p>আমাদের WordPress Theme সফলভাবে কাজ করছে।</p>
+</main>
+
+<?php get_footer(); ?>
